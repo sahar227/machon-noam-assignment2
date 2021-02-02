dc65118e56a94b67f720ee54aae1d826e66de263
sahar227 <sahar22798@gmail.com>
2021-02-02 16:09:50 +0200

    Added validations to form and send request on submit

--- Web Frontend/script.js
+++ Web Frontend/script.js
@@ -1,11 +1,45 @@
 const statusElement = document.getElementById('status');
+const password = document.getElementById("password")
+const confirm_password = document.getElementById("confirm_password");
 
-const submitForm = (e) => {
+const endpointAddress = 'http://localhost:52308/auth'
+const submitForm = async (e) => {
     e.preventDefault();
+    if(!validatePassword())
+        return;
     const formData = new FormData(e.target);
-    console.log(formData.get('email'));
+    const response = await sendRequest({
+        email: formData.get('email'),
+        password: formData.get('password'),
+        repeatedPassword: formData.get('confirm_password')
+    });
+
+
+    console.log(response);
 }
 
 const changeStatus = (message) => {
     statusElement.innerHTML = message;
 }
+
+// validate password confirmation matches password
+const validatePassword = () =>{
+    if(password.value != confirm_password.value) {
+      confirm_password.setCustomValidity("Passwords Don't Match");
+      return false;
+    }
+    confirm_password.setCustomValidity('');
+    return true;
+  }
+
+  const sendRequest = async (request) => {
+    const response = await fetch(endpointAddress, {
+        method: 'POST',
+        headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(request)
+    });
+    return response;
+  }
