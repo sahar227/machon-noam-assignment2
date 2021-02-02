5ef9c303820c84a9fce96df0fe619d5c87cf2b32
sahar227 <sahar22798@gmail.com>
2021-02-02 12:47:36 +0200

    Initial commit

---
+++ Backend/AuthApi/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+
+        public AuthController()
+        {
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
